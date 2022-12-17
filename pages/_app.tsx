074344d2bee14809de5765dd7a6cd43651bfdc86
bfamchon <baptiste.famchon@thetribe.io>
2022-12-17 01:03:23 +0100

    :sparkles: add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
+
 config.autoAddCss = false;
 
 import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <ThemeProvider enableSystem={true} attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <Analytics />
+    </>
   );
 }
 
