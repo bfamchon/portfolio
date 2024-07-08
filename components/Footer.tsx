63f449b32b7b9b7bcb58f87193deeb4f1d0d60ce
FAMCHON Baptiste <bfamchon@gmail.com>
2024-07-08 10:41:30 +0200

    add cv download

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,18 @@
 import { faGithub, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
 import { SocialLink } from './SocialLink';
 
 const Footer = () => {
   return (
-    <footer className="px-4 sm:px-6 py-6 mt-24">
+    <footer className="px-4 py-6 mt-24 sm:px-6">
       <div id="socials" className="flex justify-center my-8">
         <SocialLink href="https://www.linkedin.com/in/baptiste-famchon/" icon={faLinkedinIn} />
         <SocialLink href="https://github.com/bfamchon" icon={faGithub} />
         <SocialLink href="https://twitter.com/bfamchon" icon={faTwitter} />
+        <SocialLink href="/cv.pdf" icon={faFilePdf} download='CV_FAMCHON_BAPTISTE' />
       </div>
-      <div className="text-center text-sm text-gray-500">
-        <span className="dark:text-gray-100 text-gray-900 font-bold text-lg mr-2">{'{ bfamchon }'}</span>{' '}
+      <div className="text-sm text-center text-gray-500">
+        <span className="mr-2 text-lg font-bold text-gray-900 dark:text-gray-100">{'{ bfamchon }'}</span>{' '}
         &copy; {new Date().getFullYear()} All Rights Reserved
       </div>
     </footer>
--- components/SocialLink.tsx
+++ components/SocialLink.tsx
@@ -1,12 +1,13 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export const SocialLink = ({ href, icon }: { href: string; icon: IconProp }) => (
+export const SocialLink = ({ href, icon, download }: Partial<HTMLAnchorElement> & { icon: IconProp }) => (
   <a
     href={href}
     target="_blank"
     rel="noreferrer"
-    className=" flex items-center justify-center w-8 h-8 p-2 mx-2 transition duration-300 ring-indigo-500 ring-2 rounded-full hover:bg-indigo-500 hover:ring-indigo-500 hover:shadow-md hover:text-white"
+    download={download}
+    className="flex items-center justify-center w-8 h-8 p-2 mx-2 transition duration-300 rounded-full ring-indigo-500 ring-2 hover:bg-indigo-500 hover:ring-indigo-500 hover:shadow-md hover:text-white"
   >
     <FontAwesomeIcon icon={icon} />
   </a>
